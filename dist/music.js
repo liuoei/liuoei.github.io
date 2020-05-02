9feccab7cd92a8d36bad5da1f9f368518ba9de09
liuoei <liusiqirg@163.com>
2020-05-02 10:10:56 +0800

    Site updated: 2020-05-02 10:10:55

--- dist/music.js
+++ dist/music.js
@@ -12,19 +12,19 @@ const ap = new APlayer({
       {
         name: '发如雪',
         artist: '周杰伦',
-        url: 'http://mp.111ttt.cn/mp3music/118990.mp3',
+        url: 'https://onedrive.gimhoy.com/sharepoint/aHR0cHM6Ly9odXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvY25yZ2xzcV9odXN0X2VkdV9jbi9FY0VVVkZXV1ZEbEVuc3dOanJOanBZTUJVb3M0OHkxYnBiZUdzVmpOV2c0QzBnP2U9aHdkdE5a.mp3',
         cover: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588349252971&di=6225d2e82139d2a5fe2d3bc286f9deef&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201301%2F18%2F20130118235943_BZckY.thumb.700_0.jpeg',
       },
       {
         name: '夜的第七章',
         artist: '周杰伦&潘儿',
-        url: 'http://antiserver.kuwo.cn/anti.s?useless=resource&format=mp3&rid=MUSIC_228907&response=res&type=convert_url&br=320kmp3&file=link.mp3',
+        url: 'https://onedrive.gimhoy.com/sharepoint/aHR0cHM6Ly9odXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvY25yZ2xzcV9odXN0X2VkdV9jbi9FZWszcFFtcmMzaElpYTNIazRUclBXRUJDdEYzZGhLd1U0VGdXdEJWUHQzOFl3P2U9TWFMQVFz.mp3',
         cover: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3515339255,299950061&fm=26&gp=0.jpg',
       },
       {
         name: '一路向北',
         artist: '周杰伦',
-        url: 'http://sr.sycdn.kuwo.cn/e7f682cb8fb2bf7cecdb3ad3c867c7d4/5eac25f3/resource/n3/38/86/2397999236.mp3',
+        url: 'https://onedrive.gimhoy.com/sharepoint/aHR0cHM6Ly9odXN0ZWR1Y24tbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvY25yZ2xzcV9odXN0X2VkdV9jbi9FWkZQZnVmRnFQOUZxYXFzeTZoN2tYc0JuUmJ3VXdDT2JMaXpBOXB0UUc1REVBP2U9MzFxZDdx.mp3',
         cover: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1588349252971&di=6225d2e82139d2a5fe2d3bc286f9deef&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201301%2F18%2F20130118235943_BZckY.thumb.700_0.jpeg',
       },
       {
@@ -32,12 +32,6 @@ const ap = new APlayer({
         artist: 'Guns N \' Roses' ,
         url: 'https://other-web-rc01-sycdn.kuwo.cn/3d30a2b39f8b8befab51fd9c8f09516a/5eac27d5/resource/n2/1/56/3330573613.mp3',
         cover: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1604755518,1099120615&fm=15&gp=0.jpg',
-      },
-      {
-        name: 'Stairway to Heaven',
-        artist: 'Led Zeppelin' ,
-        url: 'https://rl01-sycdn.kuwo.cn/2f93cb29d90c03bde8e64fdd2bd5f370/5eac294f/resource/n3/60/5/3619797768.mp3',
-        cover: 'https://i0.wp.com/thetmca.com/files/2016/06/stairwaytoheaven.jpg?resize=225%2C225',
       },
       {
         name: '将故事写成我们',
